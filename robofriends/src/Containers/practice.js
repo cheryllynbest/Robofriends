84dd8ee1bfdb0b9c901b9c2c20327a047a428ea9
Cheryl Best <cheryllynbest@gmail.com>
2019-04-21 15:26:18 -0600

    Initial Commit

---
+++ robofriends/src/Containers/practice.js
@@ -0,0 +1,32 @@
+
+
+
+
+// function doThisShit() {
+//     console.log("Happy");
+// }
+
+// function callBack( cB ) {
+//     cB()
+// }
+
+// callBack(doThisShit);
+
+
+// setTimeout(() => { 
+//     console.log("Happy");
+// }  , 5000)
+
+
+// const doThisShit = () => console.log("Happy");
+
+// const callBack = (cB) => cB();
+
+// doThisShit();
+
+
+function practiceThis() {
+    console.log(this);
+}
+
+practiceThis();
